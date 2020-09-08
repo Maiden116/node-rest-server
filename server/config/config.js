eb1cf5f24851f698dacede487277121c8af286c9
Emmanuel Leiva <emma_lthe@hotmail.com>
2020-09-08 19:06:32 -0300

    Configure database on heroku env variable

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = "mongodb+srv://maiden116:vYKZHnMA8GssNVfE@cluster0.cfizo.mongodb.net/cafe?retryWrites=true&w=majority";
+    urlDB = process.env.dburl;
 }
 process.env.URLDB = urlDB;
