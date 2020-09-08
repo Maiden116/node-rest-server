1156c586dca3cdfaaeba02ebe51af57368c4f28a
Emmanuel Leiva <emma_lthe@hotmail.com>
2020-09-08 19:21:35 -0300

    quito comentario

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,6 @@ process.env.PORT = process.env.PORT || 3000
     //Env
     //====================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
-console.log(process.env.MONGO_URI);
 let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = "mongodb+srv://maiden116:vYKZHnMA8GssNVfE@cluster0.cfizo.mongodb.net/cafe?retryWrites=true&w=majority";
