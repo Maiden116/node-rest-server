73be1f4ab1d3ff97b8b5db76ebfa3fcdddb8bec8
Emmanuel Leiva <emma_lthe@hotmail.com>
2020-09-08 19:25:12 -0300

    quito url productiva de local

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ process.env.PORT = process.env.PORT || 3000
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = "mongodb+srv://maiden116:vYKZHnMA8GssNVfE@cluster0.cfizo.mongodb.net/cafe?retryWrites=true&w=majority";
+    urlDB = 'mongodb://localhost:27017/cafe';;
 } else {
     urlDB = process.env.MONGO_URI;
 }
