84a99c68417ddc4cc029c4368f83a7fea7c2a3c5
Emmanuel Leiva <emma_lthe@hotmail.com>
2020-09-08 19:20:10 -0300

    fix urldb

--- server/config/config.js
+++ server/config/config.js
@@ -6,11 +6,11 @@ process.env.PORT = process.env.PORT || 3000
     //Env
     //====================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
-console.log(process.env);
+console.log(process.env.MONGO_URI);
 let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = "mongodb+srv://maiden116:vYKZHnMA8GssNVfE@cluster0.cfizo.mongodb.net/cafe?retryWrites=true&w=majority";
 } else {
-    urlDB = 'mongodb+srv://maiden116:vYKZHnMA8GssNVfE@cluster0.cfizo.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
